f529ebe78b45d28eae5ab7aef3cc8081c3dc68d7
AgustinABR <amuriels001@iesalbarregas.es>
2026-04-28 20:36:25 +0200

    implementar generacion dinamica de interfaz en la seleccion de criaturas

--- src/main/java/com/combate/controllers/SeleccionController.java
+++ src/main/java/com/combate/controllers/SeleccionController.java
@@ -10,7 +10,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import java.io.InputStreamReader;
@@ -19,9 +19,7 @@ import java.util.Random;
 
 public class SeleccionController {
 
-    @FXML private ImageView img0, img1, img2, img3;
-    @FXML private Label info0, info1, info2, info3;
-    @FXML private VBox card0, card1, card2, card3; 
+    @FXML private FlowPane panelCriaturas; 
 
     private List<Criatura> listaPokemon;
 
@@ -32,44 +30,51 @@ public class SeleccionController {
             var is = getClass().getResourceAsStream("/data/criaturas.json");
             listaPokemon = gson.fromJson(new InputStreamReader(is), new TypeToken<List<Criatura>>(){}.getType());
 
-            configurarFicha(0, img0, info0, card0);
-            configurarFicha(1, img1, info1, card1);
-            configurarFicha(2, img2, info2, card2);
-            configurarFicha(3, img3, info3, card3);
+            generarTarjetas();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    private void configurarFicha(int index, ImageView img, Label lbl, VBox card) {
-        if (index < listaPokemon.size()) {
-            Criatura c = listaPokemon.get(index);
-            img.setImage(new Image(getClass().getResourceAsStream(c.imagen)));
+    private void generarTarjetas() {
+        panelCriaturas.getChildren().clear();
+
+        for (Criatura pokemon : listaPokemon) {
+            VBox card = new VBox(10);
+            card.setStyle("-fx-alignment: center; -fx-padding: 15; -fx-border-color: #ccc; -fx-border-radius: 10; -fx-background-color: white; -fx-cursor: hand;");
+            card.setPrefWidth(180);
+
+            ImageView img = new ImageView();
+            try {
+                img.setImage(new Image(getClass().getResourceAsStream(pokemon.imagen)));
+            } catch (Exception e) {
+                System.out.println("Error cargando imagen de: " + pokemon.nombre);
+            }
+            img.setFitHeight(120);
             img.setPreserveRatio(true);
-            card.setUserData(index); 
-            lbl.setText(c.nombre.toUpperCase() + "\n❤ " + c.vidaMax + " | ⚔ " + c.ataque + " | 🛡 " + c.defensa);
+
+            Label lblInfo = new Label(pokemon.nombre.toUpperCase() + "\n❤ " + pokemon.vidaMax + " | ⚔ " + pokemon.ataque);
+            lblInfo.setStyle("-fx-text-alignment: center; -fx-font-weight: bold;");
+
+            card.setOnMouseClicked(event -> irACombate(pokemon));
+
+            card.getChildren().addAll(img, lblInfo);
+            panelCriaturas.getChildren().add(card);
         }
     }
 
-    @FXML
-    void seleccionarPokemon(MouseEvent event) {
+    private void irACombate(Criatura elegido) {
         try {
-            VBox fuente = (VBox) event.getSource();
-            Integer index = (Integer) fuente.getUserData();
-            
-            if (index != null) {
-                Criatura elegido = listaPokemon.get(index);
-                Criatura rival = listaPokemon.get(new Random().nextInt(listaPokemon.size()));
+            Criatura rival = listaPokemon.get(new Random().nextInt(listaPokemon.size()));
 
-                FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/vistaCombate.fxml"));
-                Parent root = loader.load();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/vistaCombate.fxml"));
+            Parent root = loader.load();
 
-                CombatController controller = loader.getController();
-                controller.setDatos(elegido, rival);
+            CombatController controller = loader.getController();
+            controller.setDatos(elegido, rival);
 
-                Stage stage = (Stage) fuente.getScene().getWindow();
-                stage.setScene(new Scene(root));
-            }
+            Stage stage = (Stage) panelCriaturas.getScene().getWindow();
+            stage.setScene(new Scene(root));
         } catch (Exception e) {
             e.printStackTrace();
         }
